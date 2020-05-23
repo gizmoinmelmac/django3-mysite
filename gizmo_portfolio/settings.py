b8d983468c1c667396ed60f9d4702fd12c8c7b3a
gizmoinpythonanywhere <parafajardo@gmail.com>
2020-05-23 16:37:28 +0000

    pythonanywhere changes

--- gizmo_portfolio/settings.py
+++ gizmo_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')0ytu4x-app_hic(2st_s7#%yw9!r+h52ldnqzwe-^0s&4fv&u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gizmoinmelmac.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/' # django-storages
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
